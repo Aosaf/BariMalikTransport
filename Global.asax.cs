f45ed23229eefdc7679f6b13e8f20839367d9331
Aosaf <49658888+Aosaf@users.noreply.github.com>
2020-03-12 13:05:32 +0500

    Initial

--- Global.asax.cs
+++ Global.asax.cs
@@ -15,7 +15,6 @@ namespace BariMalikTransport
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
