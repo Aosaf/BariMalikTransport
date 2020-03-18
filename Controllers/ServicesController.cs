e6a30018d85ee877c53f1f3dddd65077ab0e4302
Aosaf <49658888+Aosaf@users.noreply.github.com>
2020-03-18 16:35:08 +0500

    MetaTags

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -17,39 +17,62 @@ namespace BariMalikTransport.Controllers
             pageInfo.URL = Url.Action("Index", "Services");
             return View(pageInfo);
         }
+        public ActionResult GeneralCargo()
+        {
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "General Cargo";
+            pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
+            pageInfo.URL = Url.Action("GeneralCargo", "Services");
+            return View(pageInfo);
+        }
+        public ActionResult FullContainerLoad()
+        {
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "Full Container Load";
+            pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
+            pageInfo.URL = Url.Action("FullContainerLoad", "Services");
+            return View(pageInfo);
+        }
         public ActionResult AutomobileTransportation()
         {
             PageInfo pageInfo = new PageInfo();
             pageInfo.Title = "Automobile Transportation";
             pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
-            pageInfo.URL = Url.Action("Index", "Services");
-            return View();
-        }
-        public ActionResult HouseholdGoods()
-        {
-            return View();
-        }
-        public ActionResult GeneralCargo()
-        {
-            return View();
+            pageInfo.URL = Url.Action("AutomobileTransportation", "Services");
+            return View(pageInfo);
         }
         public ActionResult ContainerShifting()
         {
-            return View();
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "Container Shifting";
+            pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
+            pageInfo.URL = Url.Action("ContainerShifting", "Services");
+            return View(pageInfo);
         }
         public ActionResult LocalTruckingServices()
         {
-            return View();
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "Local Trucking Services";
+            pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
+            pageInfo.URL = Url.Action("LocalTruckingServices", "Services");
+            return View(pageInfo);
         }
-        public ActionResult FullContainerLoad()
+        public ActionResult HouseholdGoods()
         {
-            return View();
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "Household Goods";
+            pageInfo.Description = "Local and Nationalwide Goods Transport Solutions all over Pakistan";
+            pageInfo.URL = Url.Action("HouseholdGoods", "Services");
+            return View(pageInfo);
         }
-
         //Contact Us
         public ActionResult ContactUs()
         {
-            return View();
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.Title = "Contact Us";
+            pageInfo.Description = "Let's Create Something Together";
+            pageInfo.URL = Url.Action("ContactUs", "Services");
+            return View(pageInfo);
         }
 
     }
